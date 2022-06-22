22483367dc42870cbecb0865af5cb171235ccfd9
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2022-06-22 22:23:14 +0300

    changed vector to normal

--- src/components/Ray.ts
+++ src/components/Ray.ts
@@ -1,21 +1,20 @@
 import Point from "./Point";
-import Vector from "./Vector";
+import Normal from "./Normal";
 
 export default class Ray {
-    private _direction: Vector;
+    private _direction: Normal;
 	private _origin: Point;
 
-	constructor(direction: Vector, origin: Point) {
-		this._direction = direction.normalize();
+	constructor(direction: Normal, origin: Point) {
+		this._direction = direction;
 		this._origin = origin;
 	}
 
     public get origin(): Point { return this._origin };
-    public get direction(): Vector { return this._direction };
+    public get direction(): Normal { return this._direction };
 
 	/* vec(o) + vec(d) * t, t - number of units from o */
-	public getPointAt = (n: number): Point => {
-		return this._origin.add(this._direction.mul(n));
+	public getPointAt = (t: number): Point => {
+		return this._origin.add(this._direction.mul(t));
 	}
-
 }
