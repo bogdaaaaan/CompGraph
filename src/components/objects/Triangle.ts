6a451b112c64cec27c043e4bb2d788ae227ec39e
bogdaaaaan <bogdan.glechkovskiy@gmail.com>
2022-06-24 02:34:34 +0300

    getting normal rework

--- src/components/objects/Triangle.ts
+++ src/components/objects/Triangle.ts
@@ -15,10 +15,6 @@ export default class Triangle implements IObject {
     private _n2: Vector;
     private _n3: Vector;
 
-    private _v: number;
-    private _u: number;
-    private _eps: number;
-
     constructor(a: Point, b: Point, c: Point, n1: Vector, n2: Vector, n3: Vector) {
         this._v1 = a;
         this._v2 = b;
@@ -27,60 +23,69 @@ export default class Triangle implements IObject {
         this._n1 = n1;
         this._n2 = n2;
         this._n3 = n3;
-
-        this._eps = 0.000001;
     }
 
+    /* Möller–Trumbore algorithm */
     intersectionWith = (ray: Ray): number => {
         const orig: Point = ray.origin;
         const dir: Vector = ray.direction;
         
+        /* find two vectors that share vertex #1 */
         const edge1: Vector = this._v2.sub(this._v1);
         const edge2: Vector = this._v3.sub(this._v1);
 
         const pvec: Vector = dir.cross(edge2);
         const det: number = edge1.dot(pvec)
 
-        if (det === 0){
-            return null;
-        }
+        /* if determinant is zero, ray lies in plane of triangle */
+        if (det === 0) return null;
+
         const inv_det: number = 1 / det;
-
         const tvec: Vector = orig.sub(this._v1);
-        
-        let u: number = tvec.dot(pvec) * inv_det;
+        const u: number = tvec.dot(pvec) * inv_det;
 
-        if (u < 0 || u > 1) {
-            return null;
-        }
+        if (u < 0 || u > 1) return null;
 
         const qvec: Vector = tvec.cross(edge1);
-        let v: number = dir.dot(qvec) * inv_det;
+        const v: number = dir.dot(qvec) * inv_det;
 
-        if (v < 0 || u + v > 1) {
-            return null;
-        }
+        if (v < 0 || u + v > 1) return null;
 
-        let t: number = edge2.dot(qvec) * inv_det;
+        /* calculate t parameter */
+        const t: number = edge2.dot(qvec) * inv_det;
 
-        this._u = u;
-        this._v = v;
-
-        if (t >= 0) {
-            return t;
-        } else {
-            return null;
-        }
+        return t >= 0 ? t : null;
     }
 
     getNormalAtPoint = (p: Point): Normal => {
+        /* if all normals are zero, return cross product of two verticies sharing p point */
+        if (!this._n1.length() && !this._n2.length() && !this._n3.length()) {
+            const edge1: Vector = this._v2.sub(p);
+            const edge2: Vector = this._v3.sub(p);
+            return edge1.cross(edge2).toNormal();
+        }
 
-        const edge1: Vector = this._v2.sub(p);
-        const edge2: Vector = this._v3.sub(p);
-        return edge1.cross(edge2).toNormal();
-        //return this._n2.mul(this._u).add(this._n3.mul(this._v).add(this._n1.mul(1-this._v-this._u))).toNormal();  // barycentric normal
+        /* create vectors from each vertex to point and get lengths */
+        let l1: number = this._v1.sub(p).length();
+        let l2: number = this._v2.sub(p).length();
+        let l3: number = this._v3.sub(p).length();
+
+        const l_sum: number = l1 + l2 + l3;
+
+        l1 = l1 / l_sum;
+        l2 = l2 / l_sum;
+        l3 = l3 / l_sum;
+
+        /* increase normals, add them and normalize the result */
+        let a = this._n1.mul(l1)
+        let b = this._n2.mul(l2)
+        let c = this._n3.mul(l3)
+
+        const res = (a.add(b)).add(c);
+        return res.toNormal();
     }
 
+    /* transforms each point and normal depending from transform matrix */
     transform = (matrix: Matrix4x4): void => {
         this._v1 = matrix.multiplyPoint(this._v1);
         this._v2 = matrix.multiplyPoint(this._v2);
@@ -90,5 +95,4 @@ export default class Triangle implements IObject {
         this._n2 = matrix.multiplyNormal(this._n2);
         this._n3 = matrix.multiplyNormal(this._n3);
     }
-
 }
